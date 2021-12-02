3dfb87d114f2d58b9135f6a90dd4f2aef5a90e8d
piacenti <piacenti10@gmail.com>
2021-12-01 20:26:42 -0800

    day 1 better file organization

--- src/day1/Day01.kt
+++ src/day1/Day01.kt
@@ -1,3 +1,7 @@
+package day1
+
+import readInput
+
 fun main() {
     fun part1(input: List<Int>): Int {
         return input.filterIndexed { index, after ->
@@ -25,11 +29,11 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInput("day1/Day01_test")
     check(part1(testInput) == 7)
     check(part2(testInput) == 5)
 
-    val input = readInput("Day01")
+    val input = readInput("day1/Day01")
     println(part1(input))
     println(part2(input))
 }
